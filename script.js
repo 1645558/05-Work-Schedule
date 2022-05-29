91b73c3dd4a6c3c38327080ee67c8a7c9ffa0352
Rare Raptor <connorwtuncc@gmail.com>
2022-05-29 16:59:00 -0400

    made some changes to local storage so i can get each input

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var containerEL = $('.container');
 var saveBtn = $('.saveBtn');
+var inputs = [];
+var textInputs = 0;
 
-$('.text-area').css({'width': '1000px', 'height': '75px'});
+$('.text-area').css({ 'width': '1000px', 'height': '75px' });
 $('.time-area').css('border-bottom', '2px dotted');
 $('.saveBtn').css('padding', '28px');
 
-saveBtn.on('click', function() {
-    var input = $('.text-area').val();
-    console.log(input);
-    localStorage.setItem('text-area', input);
+saveBtn.on('click', function () {
+    $('.text-area').each(function () {
+        inputs[textInputs++] = $('.text-area').val();
+        console.log(inputs);
+        localStorage.setItem('text-area', inputs)
+    })
 })
 
