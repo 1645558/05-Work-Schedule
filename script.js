8a19348d238622c6b2d0eeefbced1e7ed1936b52
Rare Raptor <connorwtuncc@gmail.com>
2022-05-29 21:59:55 -0400

    trying to add displaymsg function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var containerEL = $('.container');
 var saveBtn = $('.saveBtn');
+var textArea = $('.text-area')
+var msgDiv = $('.msg')
 var inputs = [];
 var textInputs = 0;
 
@@ -7,11 +9,23 @@ $('.text-area').css({ 'width': '1000px', 'height': '75px' });
 $('.time-area').css('border-bottom', '2px dotted');
 $('.saveBtn').css('padding', '28px');
 
-saveBtn.on('click', function () {
+function displayMsg(message) {
+    msgDiv.textContent = message;
+}
+
+saveBtn.on('click', function (event) {
+    event.preventDefault()
+    if (textArea === '') {
+        displayMsg('Please add something to save on your calender!')
+    } else 
+        displayMsg('Saved to localstorage!')
     $('.text-area').each(function () {
-        inputs[textInputs++] = $('.text-area').val();
+        inputs = textArea.val();
+
+        localStorage.setItem('text-area', inputs);
+        localStorage.getItem(inputs)
         console.log(inputs);
-        localStorage.setItem('text-area', inputs)
-    })
-})
+        console.log(JSON.stringify(textArea.val()));
+    });
+});
 
