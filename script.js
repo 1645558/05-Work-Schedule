b4c1e66b0a62ed001a8387ef4867c2342ab8432a
Rare Raptor <connorwtuncc@gmail.com>
2022-05-30 14:03:00 -0400

    trying out different things for function

--- script.js
+++ script.js
@@ -3,29 +3,38 @@ var saveBtn = $('.saveBtn');
 var textArea = $('.text-area')
 var msgDiv = $('.msg')
 var inputs = [];
-var textInputs = 0;
 
+//Styling
 $('.text-area').css({ 'width': '1000px', 'height': '75px' });
 $('.time-area').css('border-bottom', '2px dotted');
 $('.saveBtn').css('padding', '28px');
 
+//Function to display message when local storage is saved
 function displayMsg(message) {
     msgDiv.textContent = message;
 }
 
-saveBtn.on('click', function (event) {
-    event.preventDefault()
-    inputs = textArea.val();
+function displayData() {
+    for (i = 0; i < inputs.length; i++)
+        var saveData = inputs;
+        JSON.parse(localStorage.getItem(saveData))
 
-        localStorage.setItem('inputs', inputs);
-        textArea.textContent = localStorage.getItem('text-area')
-        console.log(inputs);
+}
+
+//function for saving input to local storage upon save button click
+saveBtn.on('click', function () {
+    inputs = textArea.val();
+    localStorage.setItem('text-area', inputs);
+
         console.log(JSON.stringify(textArea.val()));
-        if (textArea === '') {
-            displayMsg('Please add something to save on your calender!')
+        console.log(inputs);
+
+        if (inputs === '') {
+            displayMsg('Please add something to save on your calender!');
         } else 
-            displayMsg('Saved to localstorage!')
-            console.log(displayMsg())
+            displayMsg('Saved to localstorage!');
+            console.log(displayMsg());
     });
+    displayData();
 
 
