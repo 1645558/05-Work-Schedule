53f6c42b6f34475e04d30a786149e82fef33a23f
Rare Raptor <connorwtuncc@gmail.com>
2022-05-31 19:05:17 -0400

    got local storage working, and display date at top of page

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ var saveBtn = $('.saveBtn');
 var textAreaEL = $('.text-area');
 var msgDivEL = $('.msg');
 
+//Display time at the top of the page
+var today = moment()
+$('#currentDay').text(today.format('dddd MMMM Do YYYY'))
 
 //Styling
 $('.text-area').css({ 'width': '1000px', 'height': '75px' });
 $('.time-area').css('border-bottom', '2px dotted');
 $('.saveBtn').css('padding', '28px');
 
-function displayData() {
-
-}
-
 //saving input to local storage upon save button click
 saveBtn.on('click', function () {
     var text = $(this).siblings('.text-area').val();
-    var hour = $(this).siblings('.text-area').text;
+    var hour = $(this).parent().attr('id');
+
 
     localStorage.setItem(hour, text);
     console.log(hour);
