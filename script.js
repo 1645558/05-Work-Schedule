e7bb8127997532a1002648c2d043d62456746723
Rare Raptor <connorwtuncc@gmail.com>
2022-05-30 23:18:40 -0400

    got local storage function working

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var containerEL = $('.container');
 var saveBtn = $('.saveBtn');
-var textArea = $('.text-area')
-var msgDiv = $('.msg')
-var input = {};
+var textArea = $('.text-area');
+var msgDiv = $('.msg');
+var checkLocalStorage = localStorage.getItem('text-area');
+if (checkLocalStorage) {
+    const arr = JSON.parse(checkLocalStorage)
+    console.log(arr)
+}
 
 //Styling
 $('.text-area').css({ 'width': '1000px', 'height': '75px' });
@@ -15,35 +19,23 @@ function displayMsg(message) {
 }
 
 function displayData() {
-    input = textArea.val();
-
-        var saveData = JSON.parse(localStorage.getItem('inputs')) || [];
-        saveData.push(input);
+    
 }
 
 //function for saving input to local storage upon save button click
 saveBtn.on('click', function (event) {
     event.preventDefault();
     input = textArea.val();
-    var text = textArea.length
+
+    var input = []
     
-    for (i=0; i <= text; i++) {
-        var x = textArea[i]
-        localStorage.setItem('text-area', JSON.stringify(x));
+    for (let i = 9; i != 18; i++) {
+        const test = $('#' + i).val()
+        input.push(test)
     }
 
     localStorage.setItem('text-area', JSON.stringify(input));
 
-    displayData();
-
-    console.log(JSON.stringify(input));
-    console.log(input);
-
-    if (input === '') {
-        displayMsg('Please add something to save on your calender!');
-    } else
-        displayMsg('Saved to localstorage!');
-    console.log(displayMsg());
 });
 
 
