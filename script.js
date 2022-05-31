32594b68f92511b4e188dd820650900d48901f37
Rare Raptor <connorwtuncc@gmail.com>
2022-05-31 18:09:18 -0400

    started over

--- script.js
+++ script.js
@@ -2,44 +2,19 @@ var containerEL = $('.container');
 var saveBtn = $('.saveBtn');
 var textArea = $('.text-area');
 var msgDiv = $('.msg');
-var checkLocalStorage = localStorage.getItem('text-area');
-if (checkLocalStorage) {
-    const arr = JSON.parse(checkLocalStorage);
-    console.log(arr);
-};
+
 
 //Styling
 $('.text-area').css({ 'width': '1000px', 'height': '75px' });
 $('.time-area').css('border-bottom', '2px dotted');
 $('.saveBtn').css('padding', '28px');
 
-//display message when local storage is saved
-function displayMsg(message) {
-    msgDiv.textContent = message;
-}
-
-//Retrieve from local storage and display in textarea
-window.onload = function() {
-    const text = localStorage.getItem('text-area')
-    if (text !== null) {
-        
-    }
-}
-
 //saving input to local storage upon save button click
-saveBtn.on('click', function (event) {
-    event.preventDefault();
-    input = textArea.val();
+saveBtn.on('click', function () {
+    var text = containerEL.find('.text-area').val();
+    var hour = $('textarea').closest().attr('id');
 
-    var input = []
-    
-    for (let i = 9; i != 18; i++) {
-        const test = $('#' + i).val()
-        input.push(test);
-    }
-
-    localStorage.setItem('text-area', JSON.stringify(input));
-
+    localStorage.setItem(hour, text);
+    console.log(hour);
 });
-
-
+ 
